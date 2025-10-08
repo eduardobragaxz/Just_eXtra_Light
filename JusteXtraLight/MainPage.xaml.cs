a321f16e68af712f80539028c961fd142fe72b58
Eduardo Braga <braga.eduardoTL@outlook.com>
2025-10-08 18:37:19 -0300

    Remove djxl and more

--- JusteXtraLight/MainPage.xaml.cs
+++ JusteXtraLight/MainPage.xaml.cs
@@ -1,154 +1,30 @@
-using System;
-using System.Linq;
-using System.Text;
-using Microsoft.UI;
-using Windows.Storage;
 using Microsoft.UI.Xaml;
-using System.Diagnostics;
-using System.ComponentModel;
-using System.Threading.Tasks;
-using Microsoft.UI.Xaml.Media;
-using Windows.Storage.Pickers;
 using Microsoft.UI.Xaml.Controls;
+using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Collections.ObjectModel;
-using Windows.Storage.FileProperties;
-using Microsoft.UI.Xaml.Media.Imaging;
-using System.Runtime.CompilerServices;
-using Microsoft.Windows.AppNotifications;
-using Windows.ApplicationModel.DataTransfer;
-using Microsoft.Windows.ApplicationModel.Resources;
-using MApplicationData = Microsoft.Windows.Storage.ApplicationData;
-using Microsoft.Windows.Storage.Pickers;
-using System.Runtime.InteropServices;
+using Windows.Storage;
+using System.Threading.Tasks;
 
 namespace JustExtraLight;
 
 public sealed partial class MainPage : Page
 {
-    bool canDropImages;
-    bool conversionSuccessful;
-    StorageFolder? conversionsFolder;
-    StorageFolder? tempFolder;
-    readonly ImmutableArray<string> types;
-    readonly ResourceLoader resourceLoader;
-    readonly ObservableCollection<string> images;
+    readonly MainPageViewModel viewModel;
     public MainPage()
     {
         InitializeComponent();
-
-        images = [];
-        images.CollectionChanged += Images_CollectionChanged;
-        canDropImages = true;
-        resourceLoader = new();
-        conversionSuccessful = true;
-        types = ImmutableArray.Create(".exr", ".gif", ".jpg", ".jpeg", ".pam", ".pgm", ".ppm", ".pfm", ".pgx", ".png", ".apng");
+        viewModel = new();
     }
 
-    private void Images_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-    {
-        ClearListButton.IsEnabled = ConvertButton.IsEnabled = images.Count != 0;
-    }
     private async void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        await PageLoaded();
+    }
+    private async Task PageLoaded()
     {
         App.MWindow!.SetTitleBar(AppTitleBar);
         StorageFolder storageFolder = Microsoft.Windows.Storage.ApplicationData.GetDefault().TemporaryFolder;
         IReadOnlyList<StorageFolder> folders = await storageFolder.GetFoldersAsync();
-        tempFolder = await storageFolder.CreateFolderAsync($"TempStorage{folders.Count}");
-    }
-    private async void AddFolderButton_Click(object sender, RoutedEventArgs e)
-    {
-        await AddFolderImages();
-    }
-    private async Task AddFolderImages()
-    {
-        Microsoft.Windows.Storage.Pickers.FolderPicker folderPicker = new(App.MWindow!.AppWindow.Id);
-        PickFolderResult result = await folderPicker.PickSingleFolderAsync();
-
-        if (result is not null)
-        {
-            StorageFolder storageFolder = await StorageFolder.GetFolderFromPathAsync(result.Path);
-            IReadOnlyList<StorageFile> files = await storageFolder.GetFilesAsync();
-
-            if (files.Count != 0)
-            {
-                foreach (StorageFile file in files)
-                {
-                    if (types.Contains(file.FileType))
-                    {
-                        if (await TryToCopyImageToTempFolder(file))
-                        {
-                            TryAddImageToList(file);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    private async void AddImagesButton_Click(object sender, RoutedEventArgs e)
-    {
-        await AddImages();
-    }
-    private async Task AddImages()
-    {
-        Microsoft.Windows.Storage.Pickers.FileOpenPicker fileOpenPicker = new(App.MWindow!.AppWindow.Id);
-
-        foreach (string type in types)
-        {
-            fileOpenPicker.FileTypeFilter.Add(type);
-        }
-
-        IReadOnlyList<PickFileResult> results = await fileOpenPicker.PickMultipleFilesAsync();
-
-        if (results is not null && results.Count != 0)
-        {
-            StorageFolder storageFolder = Microsoft.Windows.Storage.ApplicationData.GetDefault().TemporaryFolder;
-
-            foreach (PickFileResult result in results)
-            {
-                StorageFile file = await StorageFile.GetFileFromPathAsync(result.Path);
-
-                if (await TryToCopyImageToTempFolder(file))
-                {
-                    TryAddImageToList(file);
-                }
-            }
-        }
-    }
-    private async Task<bool> TryToCopyImageToTempFolder(StorageFile file)
-    {
-        try
-        {
-            await file.CopyAsync(tempFolder);
-            return true;
-        }
-        catch (COMException)
-        {
-            return false;
-        }
-    }
-    private void TryAddImageToList(StorageFile file)
-    {
-        images.Add(file.Path);
-    }
-    private async void ConvertButton_Click(object sender, RoutedEventArgs e)
-    {
-        await DeleteFilesAfterConversion();
-    }
-    private async Task DeleteFilesAfterConversion()
-    {
-        IReadOnlyList<StorageFile> files = await tempFolder!.GetFilesAsync();
-        foreach (StorageFile file in files)
-        {
-            await file.DeleteAsync();
-        }
-
-        images.Clear();
-    }
-
-    private async void ClearListButton_Click(object sender, RoutedEventArgs e)
-    {
-        await DeleteFilesAfterConversion();
+        viewModel.TempFolder = await storageFolder.CreateFolderAsync($"TempStorage{folders.Count}");
     }
 }
---
+++ JusteXtraLight/MainPageViewModel.cs
@@ -0,0 +1,327 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.Windows.ApplicationModel.Resources;
+using Microsoft.Windows.Storage.Pickers;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace JustExtraLight;
+
+public sealed partial class MainPageViewModel : INotifyPropertyChanged
+{
+    public string Arguments
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    public bool IsConversionInProgress
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    public bool ConversionCompleted
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    public bool EnableAddButtons
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    } = true;
+    public bool EnableConvertButton
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    public bool EnableSaveButton
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    public bool EnableClearButton
+    {
+        get;
+        set
+        {
+            if (value != field)
+            {
+                field = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+    public StorageFolder? TempFolder { get; set; }
+    readonly ImmutableArray<string> types;
+    readonly ResourceLoader resourceLoader;
+    public ObservableCollection<StorageFile> ImagesList { get; set; }
+    
+    public MainPageViewModel()
+    {
+        Arguments = "";
+        ImagesList = [];
+        ImagesList.CollectionChanged += Images_CollectionChanged;
+        resourceLoader = new();
+        types = ImmutableArray.Create(".exr", ".gif", ".jpg", ".jpeg", ".pam", ".pgm", ".ppm", ".pfm", ".pgx", ".png", ".apng");
+    }
+    private void Images_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        EnableConvertButton = EnableClearButton = ImagesList.Count != 0;
+    }
+    public async void AddFolderButton_Click(object sender, RoutedEventArgs e)
+    {
+        await AddFolderImages();
+    }
+    private async Task AddFolderImages()
+    {
+        Microsoft.Windows.Storage.Pickers.FolderPicker folderPicker = new(App.MWindow!.AppWindow.Id);
+        PickFolderResult result = await folderPicker.PickSingleFolderAsync();
+
+        if (result is not null)
+        {
+            StorageFolder storageFolder = await StorageFolder.GetFolderFromPathAsync(result.Path);
+            IReadOnlyList<StorageFile> files = await storageFolder.GetFilesAsync();
+
+            if (files.Count != 0)
+            {
+                foreach (StorageFile file in files)
+                {
+                    if (types.Contains(file.FileType))
+                    {
+                        if (await TryToCopyImageToTempFolder(file))
+                        {
+                            TryAddImageToList(file);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public async void AddImagesButton_Click(object sender, RoutedEventArgs e)
+    {
+        await AddImages();
+    }
+    private async Task AddImages()
+    {
+        Microsoft.Windows.Storage.Pickers.FileOpenPicker fileOpenPicker = new(App.MWindow!.AppWindow.Id);
+
+        foreach (string type in types)
+        {
+            fileOpenPicker.FileTypeFilter.Add(type);
+        }
+
+        IReadOnlyList<PickFileResult> results = await fileOpenPicker.PickMultipleFilesAsync();
+
+        if (results is not null && results.Count != 0)
+        {
+            StorageFolder storageFolder = Microsoft.Windows.Storage.ApplicationData.GetDefault().TemporaryFolder;
+
+            foreach (PickFileResult result in results)
+            {
+                StorageFile file = await StorageFile.GetFileFromPathAsync(result.Path);
+
+                if (await TryToCopyImageToTempFolder(file))
+                {
+                    TryAddImageToList(file);
+                }
+            }
+        }
+    }
+    private async Task<bool> TryToCopyImageToTempFolder(StorageFile file)
+    {
+        try
+        {
+            await file.CopyAsync(TempFolder);
+            return true;
+        }
+        catch (COMException)
+        {
+            return false;
+        }
+    }
+    private void TryAddImageToList(StorageFile file)
+    {
+        ImagesList.Add(file);
+    }
+    public async void ConvertButton_Click(object sender, RoutedEventArgs e)
+    {
+        await ConvertImages();
+    }
+    private async Task ConvertImages()
+    {
+        if (Arguments == "" || Arguments != "" && Arguments[0..2] == "--")
+        {
+            EnableAddButtons = EnableConvertButton = EnableSaveButton = EnableClearButton = false;
+
+            string fullPath = $@"{Windows.ApplicationModel.Package.Current.InstalledPath}\Assets\Program\cjxl.exe";
+            ProcessStartInfo processStart = new(fullPath)
+            {
+                CreateNoWindow = true
+            };
+
+            StorageFolder convertedImagesFolder = await TempFolder!.CreateFolderAsync("ConvertedImages", CreationCollisionOption.ReplaceExisting);
+
+            await Task.Run(async () =>
+            {
+                IReadOnlyList<StorageFile> files = await TempFolder.GetFilesAsync();
+
+                foreach (StorageFile file in files)
+                {
+                    string newName = FixFileName(file.DisplayName);
+                    await file.RenameAsync(newName);
+                    string finalArguments = $@"{Arguments} {TempFolder.Path}\{newName}{file.FileType} {convertedImagesFolder.Path}\{newName}.jxl";
+                    processStart.Arguments = finalArguments;
+
+                    Process? process = Process.Start(processStart);
+                    if (process is not null)
+                    {
+                        process.WaitForExit();
+                        process.Close();
+                        process.Dispose();
+                    }
+                    else
+                    {
+
+                    }
+
+                    //int count = 0;
+
+                    ////can't rely on automatic name collision handling because it adds spaces in the file name
+                    ////while (localFolderImages.Any(file => file.DisplayName.Equals(newName, StringComparison.OrdinalIgnoreCase)))
+                    ////{
+                    ////    count++;
+                    ////}
+
+                    //if (count != 0)
+                    //{
+                    //    newName += $"{count}";
+                    //}
+                }
+            });
+
+            EnableSaveButton = EnableClearButton = true;
+        }
+
+        static string FixFileName(string displayName)
+        {
+            StringBuilder newName = new(displayName, displayName.Length);
+
+            if (displayName.Contains(' '))
+            {
+                newName.Replace(' ', '_');
+            }
+
+            if (displayName.Contains('-'))
+            {
+                newName.Replace('-', '_');
+            }
+
+            if (newName.Length >= 150)
+            {
+                int difference = newName.Length - 150;
+                newName.Remove(149, difference);
+            }
+
+            while (newName[^1] == '.' || newName[^1] == '-' || newName[^1] == '_')
+            {
+                newName.Remove(newName.Length - 1, 1);
+            }
+
+            return $"{newName}";
+        }
+    }
+    public async void ClearListButton_Click(object sender, RoutedEventArgs e)
+    {
+        await DeleteFilesAfterConversion();
+    }
+    private async Task DeleteFilesAfterConversion()
+    {
+        IReadOnlyList<StorageFile> files = await TempFolder!.GetFilesAsync();
+        foreach (StorageFile file in files)
+        {
+            await file.DeleteAsync();
+        }
+
+        EnableAddButtons = true;
+        EnableConvertButton = EnableSaveButton = EnableClearButton = false;
+        ImagesList.Clear();
+    }
+    public async void SaveImagesButton_Click(object sender, RoutedEventArgs e)
+    {
+        StorageFolder convertedImagesFolder = await TempFolder!.GetFolderAsync("ConvertedImages");
+
+        Microsoft.Windows.Storage.Pickers.FolderPicker folderPicker = new(App.MWindow!.AppWindow.Id);
+        PickFolderResult result = await folderPicker.PickSingleFolderAsync();
+
+        if (result is not null)
+        {
+            StorageFolder storageFolder = await StorageFolder.GetFolderFromPathAsync(result.Path);
+            IReadOnlyList<StorageFile> files = await convertedImagesFolder.GetFilesAsync();
+
+            foreach (StorageFile file in files)
+            {
+                await file.MoveAsync(storageFolder, file.DisplayName, NameCollisionOption.GenerateUniqueName);
+            }
+
+            ImagesList.Clear();
+            EnableAddButtons = true;
+            EnableConvertButton = EnableSaveButton = EnableClearButton = false;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
