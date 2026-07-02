f460ce169ddbfccd3e2b086358db57f376d56cfb
Eduardo Braga <braga.eduardoTL@outlook.com>
2026-07-02 09:10:46 -0300

    Update to 0.12

--- JusteXtraLight/ViewModels/MainPageViewModel.cs
+++ JusteXtraLight/ViewModels/MainPageViewModel.cs
@@ -348,9 +348,20 @@ public sealed partial class MainPageViewModel : INotifyPropertyChanged
 
             //string fullPath = $@"{Windows.ApplicationModel.Package.Current.InstalledPath}\Assets\Program\cjxl.exe";
             StorageFolder appFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
-            string fullPath = ConvertToJXL
-                ? $@"{appFolder.Path}\Assets\Program\cjxl.exe"
-                : $@"{appFolder.Path}\Assets\Program\djxl.exe";
+            string fullPath;
+            
+            if (System.Environment.Is64BitOperatingSystem)
+            {
+                fullPath = ConvertToJXL
+                    ? $@"{appFolder.Path}\Assets\Program\x64-windows-static\bin\cjxl.exe"
+                    : $@"{appFolder.Path}\Assets\Program\x64-windows-static\bin\djxl.exe";
+            }
+            else
+            {
+                fullPath = ConvertToJXL
+                    ? $@"{appFolder.Path}\Assets\Program\x86-windows-static\bin\cjxl.exe"
+                    : $@"{appFolder.Path}\Assets\Program\x86-windows-static\bin\djxl.exe";
+            }
 
             ProcessStartInfo processStart = new(fullPath)
             {
