d9694b69dd6b91e868ca23724876e546dd02dd75
Eduardo Braga <braga.eduardoTL@outlook.com>
2026-07-17 12:35:56 -0300

    Use optional changes

---
+++ JusteXtraLight/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml.Settings;
+using System.Threading;
+
+namespace JustExtraLight;
+
+partial class Program
+{
+    [STAThread]
+    private static int Main(string[] args)
+    {
+        XamlOptionalChanges.EnableChange(XamlChangeId.DefaultStyleOptimizations);
+        XamlOptionalChanges.EnableChange(XamlChangeId.DeferContextFlyoutInit);
+        XamlOptionalChanges.EnableChange(XamlChangeId.IconNoGridOptimization);
+        XamlOptionalChanges.EnableChange(XamlChangeId.OptimizeApplyStyles);
+
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+
+        Application.Start((p) =>
+        {
+            DispatcherQueueSynchronizationContext context = new(
+                DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+
+        return 0;
+    }
+}
