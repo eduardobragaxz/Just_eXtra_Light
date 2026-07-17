f5f442ff1f5c18fa183d463ae166b427632fcfb6
Eduardo Braga <braga.eduardoTL@outlook.com>
2026-07-17 09:28:09 -0300

    Use new optional changes

---
+++ JusteXtraLight/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Settings;
+
+namespace JustExtraLight;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        XamlOptionalChanges.EnableChange(XamlChangeId.DefaultStyleOptimizations);
+        XamlOptionalChanges.EnableChange(XamlChangeId.DeferContextFlyoutInit);
+        XamlOptionalChanges.EnableChange(XamlChangeId.IconNoGridOptimization);
+        XamlOptionalChanges.EnableChange(XamlChangeId.OptimizeApplyStyles);
+
+        Application.Start((p) =>
+        {
+            _ = new App();
+        });
+    }
+}
